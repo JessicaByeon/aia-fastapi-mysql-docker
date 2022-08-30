471c33d963afdeda449334f4e37f3c871ae138c1
JessicaByeon <jessica.byeon@gmail.com>
2022-08-30 11:20:36 +0900

    220830

--- app/utils/context.py
+++ app/utils/context.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 @dataclass
 
-class Contect:
+class Context:
     path: str
     fname: str
     train: object
@@ -14,3 +14,35 @@ class Contect:
     
     @path.setter
     def path(self, path): self._path = path
+    
+    @property
+    def fname(self) -> str: return self._fname
+    
+    @fname.setter
+    def fname(self, fname): self._fname = fname
+    
+    @property
+    def train(self) -> object: return self._train
+    
+    @train.setter
+    def train(self, train): self._train = train
+    
+    @property
+    def test(self) -> object: return self._test
+    
+    @test.setter
+    def test(self, test): self._test = test
+    
+    @property
+    def id(self) -> str: return self._id
+    
+    @id.setter
+    def id(self, id): self._id = id
+    
+    @property
+    def label(self) -> str: return self.label
+    
+    @label.setter
+    def label(self, label) : self._setter = label
+    
+    
